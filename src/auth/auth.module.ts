28fc8459a3930f5d1eab9ec9710c56559d222428
gevaghajanyan <Gevorg100%>
2020-04-28 00:34:27 +0400

    [+] add Auth.guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,22 +1,29 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     UsersModule,
+    PassportModule.register({defaultStrategy: 'jwt'}),
     JwtModule.register({
-      secret: 'jwtConstants.secret',
+      secret: process.env.JWT_SECRET_KEY,
       signOptions: { expiresIn: '60s' },
     }),
   ],
   providers: [
     AuthService,
+    JwtStrategy,
   ],
   controllers: [AuthController],
+  exports: [
+    UsersModule,
+  ]
 })
 export class AuthModule {
 }
